c7debebc73c97fea95f450bea9fae57d8385a918
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-28 17:10:43 +0100

    script.js 3 added || script.js setted

--- script.js
+++ script.js
@@ -99,4 +99,61 @@ function rand(max) {
         }
       }
     }
-};
+    function defineMaze() {
+        var isComp = false;
+        var move = false;
+        var cellsVisited = 1;
+        var numLoops = 0;
+        var maxLoops = 0;
+        var pos = {
+          x: 0,
+          y: 0
+        };
+        var numCells = width * height;
+        while (!isComp) {
+          move = false;
+          mazeMap[pos.x][pos.y].visited = true;
+    
+          if (numLoops >= maxLoops) {
+            shuffle(dirs);
+            maxLoops = Math.round(rand(height / 8));
+            numLoops = 0;
+          }
+          numLoops++;
+          for (index = 0; index < dirs.length; index++) {
+            var direction = dirs[index];
+            var nx = pos.x + modDir[direction].x;
+            var ny = pos.y + modDir[direction].y;
+    
+            if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
+              //Check if the tile is already visited
+              if (!mazeMap[nx][ny].visited) {
+                //Carve through walls from this tile to next
+                mazeMap[pos.x][pos.y][direction] = true;
+                mazeMap[nx][ny][modDir[direction].o] = true;
+    
+                //Set Currentcell as next cells Prior visited
+                mazeMap[nx][ny].priorPos = pos;
+                //Update Cell position to newly visited location
+                pos = {
+                  x: nx,
+                  y: ny
+                };
+                cellsVisited++;
+                //Recursively call this method on the next tile
+                move = true;
+                break;
+              }
+            }
+          }
+          if (!move) {
+            //  If it failed to find a direction,
+            //  move the current position back to the prior cell and Recall the method.
+            pos = mazeMap[pos.x][pos.y].priorPos;
+          }
+          if (numCells == cellsVisited) {
+            isComp = true;
+          }
+        }
+      }
+    }
