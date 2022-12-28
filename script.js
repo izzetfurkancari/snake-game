6a202c85b37451c94a24af4006f2c105317dde07
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-28 17:46:37 +0100

    script.js 5 added || script.js setted

--- script.js
+++ script.js
@@ -206,5 +206,55 @@ function rand(max) {
       defineStartEnd();
       defineMaze();
     }
-    
+    function DrawMaze(Maze, ctx, cellsize, endSprite = null) {
+        var map = Maze.map();
+        var cellSize = cellsize;
+        var drawEndMethod;
+        ctx.lineWidth = cellSize / 40;
+      
+        this.redrawMaze = function (size) {
+          cellSize = size;
+          ctx.lineWidth = cellSize / 50;
+          drawMap();
+          drawEndMethod();
+        };
+      
+        function drawCell(xCord, yCord, cell) {
+          var x = xCord * cellSize;
+          var y = yCord * cellSize;
+      
+          if (cell.n == false) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x + cellSize, y);
+            ctx.stroke();
+          }
+          if (cell.s === false) {
+            ctx.beginPath();
+            ctx.moveTo(x, y + cellSize);
+            ctx.lineTo(x + cellSize, y + cellSize);
+            ctx.stroke();
+          }
+          if (cell.e === false) {
+            ctx.beginPath();
+            ctx.moveTo(x + cellSize, y);
+            ctx.lineTo(x + cellSize, y + cellSize);
+            ctx.stroke();
+          }
+          if (cell.w === false) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x, y + cellSize);
+            ctx.stroke();
+          }
+        }
+      
+        function drawMap() {
+          for (x = 0; x < map.length; x++) {
+            for (y = 0; y < map[x].length; y++) {
+              drawCell(x, y, map[x][y]);
+            }
+          }
+        }
+    }
     
