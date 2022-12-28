070e4488d792cfbdd7fba56628ccd3c5f5ecb174
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-28 17:33:18 +0100

    script.js 4 added || script.js setted

--- script.js
+++ script.js
@@ -156,4 +156,55 @@ function rand(max) {
           }
         }
       }
+      
+    function defineStartEnd() {
+        switch (rand(4)) {
+          case 0:
+            startCoord = {
+              x: 0,
+              y: 0
+            };
+            endCoord = {
+              x: height - 1,
+              y: width - 1
+            };
+            break;
+          case 1:
+            startCoord = {
+              x: 0,
+              y: width - 1
+            };
+            endCoord = {
+              x: height - 1,
+              y: 0
+            };
+            break;
+          case 2:
+            startCoord = {
+              x: height - 1,
+              y: 0
+            };
+            endCoord = {
+              x: 0,
+              y: width - 1
+            };
+            break;
+          case 3:
+            startCoord = {
+              x: height - 1,
+              y: width - 1
+            };
+            endCoord = {
+              x: 0,
+              y: 0
+            };
+            break;
+        }
+      }
+    
+      genMap();
+      defineStartEnd();
+      defineMaze();
     }
+    
+    
