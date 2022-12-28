ff07ef261c98d1e693cf66473e9e1f83cb36903d
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-28 18:09:42 +0100

    script.js 6 added || script.js setted

--- script.js
+++ script.js
@@ -256,5 +256,85 @@ function rand(max) {
             }
           }
         }
+    
+    function drawEndFlag() {
+        var coord = Maze.endCoord();
+        var gridSize = 4;
+        var fraction = cellSize / gridSize - 2;
+        var colorSwap = true;
+        for (let y = 0; y < gridSize; y++) {
+          if (gridSize % 2 == 0) {
+            colorSwap = !colorSwap;
+          }
+          for (let x = 0; x < gridSize; x++) {
+            ctx.beginPath();
+            ctx.rect(
+              coord.x * cellSize + x * fraction + 4.5,
+              coord.y * cellSize + y * fraction + 4.5,
+              fraction,
+              fraction
+            );
+            if (colorSwap) {
+              ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
+            } else {
+              ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+            }
+            ctx.fill();
+            colorSwap = !colorSwap;
+          }
+        }
+      }
+    
+      function drawEndSprite() {
+        var offsetLeft = cellSize / 50;
+        var offsetRight = cellSize / 25;
+        var coord = Maze.endCoord();
+        ctx.drawImage(
+          endSprite,
+          2,
+          2,
+          endSprite.width,
+          endSprite.height,
+          coord.x * cellSize + offsetLeft,
+          coord.y * cellSize + offsetLeft,
+          cellSize - offsetRight,
+          cellSize - offsetRight
+        );
+      }
+      function clear() {
+        var canvasSize = cellSize * map.length;
+        ctx.clearRect(0, 0, canvasSize, canvasSize);
+      }
+    
+      if (endSprite != null) {
+        drawEndMethod = drawEndSprite;
+      } else {
+        drawEndMethod = drawEndFlag;
+      }
+      clear();
+      drawMap();
+      drawEndMethod();
     }
     
+    function Player(maze, c, _cellsize, onComplete, sprite = null) {
+      var ctx = c.getContext("2d");
+      var drawSprite;
+      var moves = 0;
+      drawSprite = drawSpriteCircle;
+      if (sprite != null) {
+        drawSprite = drawSpriteImg;
+      }
+      var player = this;
+      var map = maze.map();
+      var cellCoords = {
+        x: maze.startCoord().x,
+        y: maze.startCoord().y
+      };
+      var cellSize = _cellsize;
+      var halfCellSize = cellSize / 2;
+    
+      this.redrawPlayer = function (_cellsize) {
+        cellSize = _cellsize;
+        drawSpriteImg(cellCoords);
+      };
+    }
