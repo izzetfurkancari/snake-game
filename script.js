bcce57faa8f9aa0edc6d75601dedbb5934016616
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-28 22:33:59 +0100

    script.js 9 added || script.js setted

--- script.js
+++ script.js
@@ -504,4 +504,76 @@ function rand(max) {
         ctx.canvas.width = viewWidth - viewWidth / 100;
         ctx.canvas.height = viewWidth - viewWidth / 100;
       }
+          //Load and edit sprites
+    var completeOne = false;
+    var completeTwo = false;
+    var isComplete = () => {
+      if (completeOne === true && completeTwo === true) {
+        console.log("Runs");
+        setTimeout(function () {
+          makeMaze();
+        }, 500);
+      }
+    };
+    sprite = new Image();
+    sprite.src =
+      "https://image.ibb.co/dr1HZy/Pf_RWr3_X_Imgur.png" +
+      "?" +
+      new Date().getTime();
+    sprite.setAttribute("crossOrigin", " ");
+    sprite.onload = function () {
+      sprite = changeBrightness(1.2, sprite);
+      completeOne = true;
+      console.log(completeOne);
+      isComplete();
+    };
+  
+    finishSprite = new Image();
+    finishSprite.src =
+      "https://image.ibb.co/b9wqnJ/i_Q7m_U25_Imgur.png" +
+      "?" +
+      new Date().getTime();
+    finishSprite.setAttribute("crossOrigin", " ");
+    finishSprite.onload = function () {
+      finishSprite = changeBrightness(1.1, finishSprite);
+      completeTwo = true;
+      console.log(completeTwo);
+      isComplete();
+    };
+  };
+  
+  window.onresize = function () {
+    let viewWidth = $("#view").width();
+    let viewHeight = $("#view").height();
+    if (viewHeight < viewWidth) {
+      ctx.canvas.width = viewHeight - viewHeight / 100;
+      ctx.canvas.height = viewHeight - viewHeight / 100;
+    } else {
+      ctx.canvas.width = viewWidth - viewWidth / 100;
+      ctx.canvas.height = viewWidth - viewWidth / 100;
     }
+    cellSize = mazeCanvas.width / difficulty;
+    if (player != null) {
+      draw.redrawMaze(cellSize);
+      player.redrawPlayer(cellSize);
+    }
+  };
+  
+  function makeMaze() {
+    //document.getElementById("mazeCanvas").classList.add("border");
+    if (player != undefined) {
+      player.unbindKeyDown();
+      player = null;
+    }
+    var e = document.getElementById("diffSelect");
+    difficulty = e.options[e.selectedIndex].value;
+    cellSize = mazeCanvas.width / difficulty;
+    maze = new Maze(difficulty, difficulty);
+    draw = new DrawMaze(maze, ctx, cellSize, finishSprite);
+    player = new Player(maze, mazeCanvas, cellSize, displayVictoryMess, sprite);
+    if (document.getElementById("mazeContainer").style.opacity < "100") {
+      document.getElementById("mazeContainer").style.opacity = "100";
+    }
+  }
+    
+    
